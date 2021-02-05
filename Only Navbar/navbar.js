936aafb4f45b858768151cce2e6b201e2b22cdf5
ABHISHEK <abhi2iet@gmail.com>
2021-02-05 21:39:25 +0530

    ABHISHEK

--- Only Navbar/navbar.js
+++
@@ -1,8 +0,0 @@
-// you guyz can also set the height of side navbar if you want
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-}
-
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-}
