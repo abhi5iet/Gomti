0b6d9563facc0889ea1e2b58f92d706e22cb88df
Dee-Hegde <dee.sirsi@gmail.com>
2021-02-05 20:44:48 +0530

    learnmoremedia

---
+++ learnmore.js
@@ -0,0 +1,16 @@
+var contactus= document.querySelector("#contactus")
+var contact_sales= document.querySelector("#buton1")
+
+function colour(){
+       contactus.style.backgroundColor="rgb(4, 158, 11)"  
+       contact_sales.style.backgroundColor="rgb(4, 158, 11)"  
+   }
+   function switchColor2(){
+       contactus.style.backgroundColor="rgb(16, 180, 24)" 
+       contact_sales.style.backgroundColor="rgb(16, 180, 24)"
+   }
+   document.querySelector("button").addEventListener('mouseover', colour);
+   document.querySelector("button").addEventListener('mouseout', switchColor2)
+
+   document.querySelector("#buton1").addEventListener('mouseover', colour);
+   document.querySelector("#buton1").addEventListener('mouseout', switchColor2)
