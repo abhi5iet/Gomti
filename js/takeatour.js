91f6d3d488d1dd17f4cea5c31e7f787876c27116
harshit sanas <harrysanas140@gmail.com>
2021-02-05 11:01:57 +0530

    image add

--- js/takeatour.js
+++
@@ -1,8 +0,0 @@
-function imagefun() {
-  var Image_Id = document.getElementById("getImage");
-  if (Image_Id.src.match("imageName1.jpg")) {
-    Image_Id.src = "imageName2.jpg";
-  } else {
-    Image_Id.src = "imageName1.jpg";
-  }
-}
